634e9342475e0a8de572c97c405d1819794d9c46
IMRANDIL <aliimranadil2@gmail.com>
2022-08-02 12:27:58 +0530

    yh

--- controllers/user.js
+++ controllers/user.js
@@ -1,6 +1,31 @@
 const User = require("../models/user");
 const bigPromise = require("../middlewares/bigPromise");
+// const CustomError = require("../utils/customError");
 
-exports.userSignUp = bigPromise(async (req, res, next) => {});
+const { cookieToken } = require("../utils/cookieToken");
+
+exports.userSignUp = bigPromise(async (req, res, next) => {
+  const { name, email, password } = req.body;
+  //   if (!(name && email && password)) {
+  //     next(new CustomError("Please provide all the fields", 400));
+  //   }
+
+  if (!(name && email && password)) {
+    return res.status(400).send("Please provide all the fields");
+  }
+
+  const isUserExist = await User.findOne({ email });
+  if (isUserExist) {
+    return res.status(400).send("User already exist");
+  }
+
+  const user = await User.create({
+    name,
+    email,
+    password,
+  });
+  //send the cookie and token to the client....from utils folder.
+  cookieToken(user, res);
+});
 
 exports.userLogin = bigPromise(async (req, res, next) => {});
