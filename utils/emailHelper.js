22e4b712d77dc36076ef89e24de30fe8d64ed558
IMRANDIL <aliimranadil2@gmail.com>
2022-08-03 08:07:45 +0530

    YH

---
+++ utils/emailHelper.js
@@ -0,0 +1,26 @@
+const nodemailer = require("nodemailer");
+
+const mailHelper = async (option) => {
+  const transporter = nodemailer.createTransport({
+    host: process.env.SMTP_HOST,
+    port: process.env.SMTP_PORT,
+    auth: {
+      user: process.env.SMTP_USER,
+      pass: process.env.SMTP_PASS,
+    },
+  });
+
+  const message = {
+    from: "", //sender address..
+    to: option.email, //receiver address..
+    subject: option.subject, //subject..
+    text: option.message, //message..
+    // html: "", //html body
+  };
+
+  //send mail with defined transport object...
+
+  await transporter.sendMail(message);
+};
+
+module.exports = mailHelper;
