5f03b7eb38a2467f7a72503c8e8d22c703da87b7
IMRANDIL <aliimranadil2@gmail.com>
2022-08-04 18:21:53 +0530

    yh

--- routes/product.js
+++ routes/product.js
@@ -3,9 +3,14 @@ const { isLoggedIn } = require("../middlewares/userMiddleware");
 const { customRole } = require("../middlewares/adminMiddleware");
 const { createProduct, getAllProducts } = require("../controllers/product");
 
+//admin route..
+
 router
   .route("/admin/create")
   .post(isLoggedIn, customRole("admin"), createProduct);
+
+//public route...
+
 router.route("/").get(getAllProducts);
 
 module.exports = router;
