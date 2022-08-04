8977ea4475a2ceb83d07f8eb65a9388e90804d5c
IMRANDIL <aliimranadil2@gmail.com>
2022-08-04 17:58:00 +0530

    yh

--- controllers/product.js
+++ controllers/product.js
@@ -2,6 +2,8 @@ const Product = require("../models/product");
 const bigPromise = require("../middlewares/bigPromise");
 const cloudinary = require("cloudinary");
 
+const WhereClause = require("../utils/whereClause");
+
 exports.createProduct = bigPromise(async (req, res, next) => {
   //images...
 
@@ -45,4 +47,21 @@ exports.createProduct = bigPromise(async (req, res, next) => {
     console.log(error);
     res.status(500).json(error);
   }
+});
+
+//get all product...
+
+exports.getAllProducts = bigPromise(async (req, res, next) => {
+  const resultPerPage = req.query.limit || 6;
+
+  // const countProducts = await Product.countDocuments();
+
+  const products = new WhereClause(Product.find(), req.query)
+    .search()
+    .filterProduct();
+
+  res.status(200).json({
+    success: true,
+    products,
+  });
 });
