f627ecd301d95fa99ae3bfd079e4418256dba736
IMRANDIL <aliimranadil2@gmail.com>
2022-08-04 11:52:45 +0530

    yh

--- routes/product.js
+++ routes/product.js
@@ -2,4 +2,6 @@ const router = require("express").Router();
 const { isLoggedIn } = require("../middlewares/userMiddleware");
 const { customRole } = require("../middlewares/adminMiddleware");
 
+router.route("/create").post();
+
 module.exports = router;
