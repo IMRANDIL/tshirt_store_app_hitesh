024530a2fc36c12a0c50705d19448225f82bc014
IMRANDIL <aliimranadil2@gmail.com>
2022-08-07 07:48:07 +0530

    yh

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -12,7 +12,22 @@ exports.createOrder = bigPromise(async (req, res, next) => {
     taxAmount,
     shippingAmount,
     totalAmount,
-  } = req.Order;
+  } = req.body;
+
+  if (
+    !(
+      shippingInfo &&
+      orderItems &&
+      paymentInfo &&
+      taxAmount &&
+      shippingAmount &&
+      totalAmount
+    )
+  ) {
+    return res.status(400).json({
+      message: "Please fill all the fields",
+    });
+  }
 
   try {
     const order = await Order.create({
@@ -26,6 +41,27 @@ exports.createOrder = bigPromise(async (req, res, next) => {
     });
 
     res.status(201).json({
+      status: "success",
+      order,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json(error);
+  }
+});
+
+//get one order...
+
+exports.getOneOrder = bigPromise(async (req, res, next) => {
+  const orderId = req.params.orderId;
+  try {
+    const order = await Order.findById(orderId);
+    if (!order) {
+      return res.status(404).json({
+        message: "Order not found",
+      });
+    }
+    res.status(200).json({
       status: "success",
       order,
     });
--- routes/order.js
+++ routes/order.js
@@ -1,8 +1,9 @@
 const router = require("express").Router();
 const { isLoggedIn } = require("../middlewares/userMiddleware");
 const { customRole } = require("../middlewares/adminMiddleware");
-const { createOrder } = require("../controllers/orderController");
+const { createOrder, getOneOrder } = require("../controllers/orderController");
 
 router.route("/create").post(isLoggedIn, createOrder);
+router.route("/:orderId").get(isLoggedIn, getOneOrder);
 
 module.exports = router;
