46868e57ee69a0acd5bddc61f7b355fc29299a99
IMRANDIL <aliimranadil2@gmail.com>
2022-08-07 08:59:11 +0530

    yh

--- routes/order.js
+++ routes/order.js
@@ -5,11 +5,16 @@ const {
   createOrder,
   getOneOrder,
   getAllOrders,
+  adminOrders,
 } = require("../controllers/orderController");
 
 router.route("/create").post(isLoggedIn, createOrder);
 router.route("/all").get(isLoggedIn, getAllOrders);
 router.route("/:orderId").get(isLoggedIn, getOneOrder);
 // router.route("/all").get(isLoggedIn, getAllOrders); //beacuse of order it is giving error argument passed in must be a string..cast to object failed..
+
+//admin routes...
+
+router.route("/admin/orders").get(isLoggedIn, customRole("admin"), adminOrders);
 
 module.exports = router;
